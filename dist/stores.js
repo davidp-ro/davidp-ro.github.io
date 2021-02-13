0d0c2af57089869c91452f44282804bc00f53488
David Pescariu <davidpescariu12@gmail.com>
2021-02-13 10:01:27 +0200

    Publish

--- dist/stores.js
+++ dist/stores.js
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { writable } from "../_snowpack/pkg/svelte/store.js";
 
 export const page = writable("home");
 export const mediaQuery = writable("desktop");
--- publish.js
+++
@@ -1,5 +0,0 @@
-const ghpages = require("gh-pages");
-
-ghpages.publish('build', (err) => {
-  console.log(err);
-})
