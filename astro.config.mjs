ebff30ec65e3e9162bfe8b7aea12e1592e0c29f8
David Pescariu <davidpescariu12@gmail.com>
2023-02-18 23:17:43 +0200

    fix: Move to Github Actions Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://davidp-ro.github.io/",
-  outDir: "./docs",
   integrations: [
     tailwind({
       config: {
